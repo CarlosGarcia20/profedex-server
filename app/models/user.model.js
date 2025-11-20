e563515b67654ca3c6b352a34b4a3336dd5fc670
CarlosGarcia20 <cargarcia085@gmail.com>
2025-11-20 12:56:40 -0700

    Se eliminaron las rutas, funciones y todo lo relacionado al reigstro de usuarios para que solo los administradores lo puedan hacer

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,48 +1,5 @@
 import pool from "../config/db.js";
 
 export class userModel {
-    static async getUserByNickName({ nickname }) {
-        try {
-            const { rows } = await pool.query(
-                `SELECT nickname FROM USERS
-                WHERE  nickname = $1`,
-                [nickname]
-            )
-
-            if (rows.length > 0) {
-                return { success: true };
-            }
-
-            return { success: false };
-        } catch (error) {
-            return { success: false, error: error.message };
-        }
-    }
-
-    static async createUser({ name, nickname, password }) {
-        try {
-            const { rows } = await pool.query(
-                `INSERT INTO users (name, nickname, password)
-                    VALUES ($1, $2, $3)
-                `,
-                [name, nickname, password]
-            );
-
-            return { success: true };
-
-        } catch (error) {
-            return { success: false, error: error.message };
-        }
-    }
     
-    static async updateDataUser({ input }) {
-        try {
-            const { userId, name, nickname, password } = input;
-
-            // introducir logica para conectar a la bd
-            // const { rows }
-        } catch (error) {
-            return { success: false, error: error.message };
-        }
-    }
 }
--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -3,7 +3,3 @@ import { userController } from "../controllers/user.controller.js";
 
 export const userRouter = Router();
 
-userRouter.post('/register', userController.createUser)
-
-userRouter.post('/validate-nickname', userController.validateNickname);
-
