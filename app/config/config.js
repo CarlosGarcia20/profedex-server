5cc4c78ea4de60d58741c3ce804042601a2c10ce
CarlosGarcia20 <cargarcia085@gmail.com>
2025-11-26 12:18:37 -0700

    Se modificó el tiempo en que expira un token

--- app/config/config.js
+++ app/config/config.js
@@ -4,7 +4,7 @@ export const PORT = process.env.PORT || 4000;
 const config = {
     jwtSecret: process.env.JWT_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-    jwtExpiresIn: '5s',
+    jwtExpiresIn: '5m',
     jwtRefreshExpiresIn: '7d'
 };
 
