4f0019c36905536913efefd6b3f2b620f8420d13
CarlosGarcia20 <cargarcia085@gmail.com>
2025-11-30 18:32:54 -0700

    Añadido el método toUpperCase() en el campo 'name' y 'active' del esquema de validación de asignaturas; se asegura que los valores sean almacenados en mayúsculas.

--- app/schemas/subject.js
+++ app/schemas/subject.js
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const subjectSchema = z.object({
-    name: z.string().min(1, { error: "El nombre debe contener al menos un caracter" }),
+    name: z.string().min(1, { error: "El nombre debe contener al menos un caracter" }).toUpperCase(),
     code: z.coerce.number({ error: "El código debe ser un número" })
         .nonnegative({ error: "El código no puede ser negativo" })
         .int("El código debe ser entero")
@@ -20,15 +20,16 @@ const subjectSchema = z.object({
         .int("El semestre debe de ser enteros")
         .min(1, { error: "El semestre debe de contener al menos un cáracter" }),
     plan_year: z.coerce.number()
-           .int()
-           .min(1900, "Año muy antiguo")
-           .max(2100, "Año muy lejano"),
-    career_id: z.coerce.number({ error: "El id de la carrera tiene que ser número" })
+        .int()
+        .min(1900, "Año muy antiguo")
+        .max(2100, "Año muy lejano"),
+    major_id: z.coerce.number({ error: "El id de la carrera tiene que ser número" })
         .nonnegative({ error: "El id de la carrera no puede ser negativo" })
         .int("El id de la carrera debe de ser entero")
         .min(1, { error: "El id debe de contener al menos un caracter" }),
     active: z.string().min(1, { error: "El campo tiene que contener más de un caracter" })
-        .max(1, { error: "El campo no puede contener más de un caracter" }),
+        .max(1, { error: "El campo no puede contener más de un caracter" })
+        .toUpperCase(),
 });
 
 export function validateSubject(input) {
