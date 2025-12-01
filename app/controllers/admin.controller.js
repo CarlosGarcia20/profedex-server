76164087047af170422bb8c22670408524ae6b47
CarlosGarcia20 <cargarcia085@gmail.com>
2025-12-01 15:34:07 -0700

    Se agregó la obtencion de roles al controlador y modelos de administrador

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -1,3 +1,17 @@
+import { adminModel } from "../models/admin.model.js";
+
 export class adminController {
-    
+    static async getRoles(req, res) {
+        try {
+            const result = await adminModel.getRoles();
+
+            if (!result.success) {
+                return res.status(404).json({ message: result.message });
+            }
+
+            return res.status(200).json({ data: result.data });
+        } catch (error) {
+            return res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
 }
--- app/models/admin.model.js
+++ app/models/admin.model.js
@@ -1,5 +1,19 @@
-
+import pool from "../config/db.js"
 
 export class adminModel {
-	
+    static async getRoles() {
+        try {
+            const { rows } = await pool.query(
+                `SELECT * FROM roles`
+            );
+
+            if (rows.length == 0) {
+                return { success: false, message: "No hay roles disponibles" }
+            }
+
+            return { success: true, data: rows }
+        } catch (error) {
+            return { success: false, error }
+        }
+    }
 }
