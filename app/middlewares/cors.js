d91a5adb5f7a640518d521917a2eb5aca74e60bc
CarlosGarcia20 <cargarcia085@gmail.com>
2025-12-12 17:25:25 -0700

    Actualizacion a los cors

--- app/middlewares/cors.js
+++ app/middlewares/cors.js
@@ -1,23 +1,25 @@
-import cors from 'cors'
-import 'dotenv/config';
+import cors from 'cors';
 
 const ACCEPTED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:4000',
     process.env.FRONTEND_URL
-]
+];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
     origin: (origin, callback) => {
+        if (!origin) {
+            return callback(null, true);
+        }
 
         if (acceptedOrigins.includes(origin)) {
-            return callback(null, true)
-        }
-        
-        if (!origin) {
-            return callback(null, true)
+            return callback(null, true);
         }
 
-        return callback(new Error('Not Allowed by CORS'))
+        console.log('BLOQUEADO POR CORS. Origen recibido:', origin);
+        console.log('Orígenes aceptados:', acceptedOrigins);
+
+        return callback(new Error('Not Allowed by CORS'));
     },
     credentials: true
-})
+});
